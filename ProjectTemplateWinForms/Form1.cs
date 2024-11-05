11d9541c4c59cc4574a17236a9a7709fcc4d5d14
Shaun Stummer <shaun.stummer1@tafensw.edu.au>
2024-11-06 10:26:04 +1100

    Updated Template Sem 2 2024

--- ProjectTemplateWinForms/Form1.cs
+++ ProjectTemplateWinForms/Form1.cs
@@ -5,19 +5,10 @@ namespace ProjectTemplateWinForms
         public Form1()
         {
             InitializeComponent();
-
             // Add code below to complete the implementation to populate the listBox
-            // by reading the employee.csv file into a List of PaySlip objects, then binding this to the ListBox.
+            // by reading the employee.csv file into a List of PaySlip objects, then binding this to the DataGrid (OR ListBox).
             // CSV file format: <employee ID>, <first name>, <last name>, <hourly rate>,<taxthreshold>
-                        
-            //CHANGE THIS TO YOUR FILE HELEN
-            var fileName = @"C:\Users\User\source\repos\ProjectTemplateWinForms\ProjectTemplateWinForms\employee.csv";
-
-
-            List<CsvMap> importedRecords = CsvImporter.ImportSomeRecords(fileName);
-
-            dgEmpData.DataSource = importedRecords;
-
+            //dgEmpData.DataSource = null; //LIST of CSV Data Objects i.e. employee data
         }
 
         private void buttonCaclulate(object sender, EventArgs e)
@@ -37,5 +28,7 @@ namespace ProjectTemplateWinForms
             // Data fields expected - EmployeeId, Full Name, Hours Worked, Hourly Rate, Tax Threshold, Gross Pay, Tax, Net Pay, Superannuation
             MessageBox.Show("Save Clicked");
         }
+
+
     }
 }
